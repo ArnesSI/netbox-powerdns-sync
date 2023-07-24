4d1b9da8fe991cac31f2de7eaeb5a362160f01f8
Matej Vadnjal <matej.vadnjal@arnes.si>
2023-07-24 17:33:11 +0200

    fix api url

--- netbox_powerdns_sync/api/urls.py
+++ netbox_powerdns_sync/api/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'netbox_powerdns_sync'
 
 router = NetBoxRouter()
-router.register('api_servers', views.ApiServerViewSet)
+router.register('api-servers', views.ApiServerViewSet)
 router.register('zones', views.ZoneViewSet)
 
 urlpatterns = router.urls
